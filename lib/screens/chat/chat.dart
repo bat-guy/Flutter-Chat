99c8ea9caf17cbd3bd953b74ede5ff859a14de92
Dheeraj <parashardheeraj@yahoo.com>
2023-10-25 05:01:18 +0530

    -Emoji support added.

--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -1,3 +1,4 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mac/common/constants.dart';
 import 'package:flutter_mac/models/message.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_mac/services/storage.dart';
 import 'package:flutter_mac/services/utils.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:giphy_get/giphy_get.dart';
+import 'package:flutter/foundation.dart' as foundation;
 
 class ChatScreen extends StatefulWidget {
   final String uid;
@@ -29,8 +31,11 @@ class _ChatScreenState extends State<ChatScreen> {
   late StorageService _storageService;
   late ViewState _viewState;
   late ScrollController _scrollController;
-  var messageList = <Message?>[];
+  final _messageList = <Message?>[];
   var count = 0;
+  bool emojiShowing = false;
+  final FocusNode _focus = FocusNode();
+  var _keyboardVisible = false;
 
   @override
   initState() {
@@ -40,17 +45,19 @@ class _ChatScreenState extends State<ChatScreen> {
     _chatUtils = ChatUtils(uid: widget.uid);
     _dbService = DatabaseService(uid: widget.uid);
     _storageService = StorageService(uid: widget.uid);
+    _focus.addListener(_onFocusChange);
     _dbService.messages.listen((list) {
-      if (messageList.isEmpty) {
-        messageList.insertAll(0, list);
+      if (_messageList.isEmpty) {
+        _messageList.insertAll(0, list);
       } else {
-        if (messageList.contains(null)) messageList.remove(null);
+        if (_messageList.contains(null)) _messageList.remove(null);
         for (int i = 0; i < list.length - 1; i++) {
-          if (messageList[i]!.id != list[i].id) messageList.insert(0, list[i]);
+          if (_messageList[i]!.id != list[i].id)
+            _messageList.insert(0, list[i]);
         }
       }
       setState(() {
-        if (count == 0) messageList.add(null);
+        if (count == 0) _messageList.add(null);
         _viewState = ViewState.viewVisible;
         if (list.isNotEmpty && (list.first.isMe || count == 0)) {
           if (count == 0) count++;
@@ -69,9 +76,156 @@ class _ChatScreenState extends State<ChatScreen> {
   void dispose() {
     _scrollController.dispose();
     super.dispose();
+    _focus.removeListener(_onFocusChange);
+    _focus.dispose();
   }
 
-  //Getting the old messages from fireStore and placing the null item in the messageList
+  @override
+  Widget build(BuildContext context) {
+    _keyboardVisible = MediaQuery.of(context).viewInsets.bottom != 0;
+    return WillPopScope(
+        onWillPop: () async {
+          return _onBackButtonPressed();
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              leading: GestureDetector(
+                child: const Icon(Icons.account_circle_rounded),
+                onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const ProfileScreen())),
+              ),
+              leadingWidth: 100, // default is 56
+              title: const Text('Chat Room'),
+              actions: [
+                IconButton.filled(
+                    onPressed: () {
+                      widget._auth.signOut();
+                    },
+                    icon: const Icon(Icons.logout)),
+              ],
+            ),
+            body: switch (_viewState) {
+              ViewState.loading => const Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SpinKitCubeGrid(
+                          color: Colors.red,
+                        ),
+                        Text(
+                          'Upload Photo. Please wait...',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        )
+                      ]),
+                ),
+              ViewState.viewVisible => Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.black12,
+                        ),
+                        child: ListView.builder(
+                            itemCount: _messageList.length,
+                            controller: _scrollController,
+                            reverse: true,
+                            itemBuilder: (context, index) {
+                              return MessageWidget(msg: _messageList[index]);
+                            }),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          IconButton(
+                            icon: const Icon(Icons.emoji_emotions_outlined),
+                            onPressed: () => setState(() {
+                              if (!emojiShowing) _focus.unfocus();
+                              emojiShowing = !emojiShowing;
+                            }),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              minLines: 1,
+                              maxLines: 5,
+                              focusNode: _focus,
+                              controller: messageController,
+                              decoration: InputDecoration(
+                                  labelText: StringConstants.typeMessage,
+                                  floatingLabelBehavior:
+                                      FloatingLabelBehavior.never),
+                              textInputAction: TextInputAction.done,
+                              onSubmitted: (value) =>
+                                  _sendMessage(_scrollController),
+                            ),
+                          ),
+                          GestureDetector(
+                            child: const Icon(Icons.attach_file),
+                            onTapDown: (TapDownDetails details) async {
+                              _showPopupMenu(details.globalPosition);
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.send),
+                            onPressed: () => _sendMessage(_scrollController),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Offstage(
+                      offstage: !emojiShowing,
+                      child: SizedBox(
+                          height: 250,
+                          child: EmojiPicker(
+                            textEditingController: messageController,
+                            onBackspacePressed: _onBackspacePressed,
+                            config: Config(
+                              columns: 7,
+                              // Issue: https://github.com/flutter/flutter/issues/28894
+                              emojiSizeMax: 32 *
+                                  (foundation.defaultTargetPlatform ==
+                                          TargetPlatform.iOS
+                                      ? 1.30
+                                      : 1.0),
+                              verticalSpacing: 0,
+                              horizontalSpacing: 0,
+                              gridPadding: EdgeInsets.zero,
+                              initCategory: Category.RECENT,
+                              bgColor: const Color(0xFFF2F2F2),
+                              indicatorColor: Colors.blue,
+                              iconColor: Colors.grey,
+                              iconColorSelected: Colors.blue,
+                              backspaceColor: Colors.blue,
+                              skinToneDialogBgColor: Colors.white,
+                              skinToneIndicatorColor: Colors.grey,
+                              enableSkinTones: true,
+                              recentTabBehavior: RecentTabBehavior.RECENT,
+                              recentsLimit: 28,
+                              replaceEmojiOnLimitExceed: false,
+                              noRecents: const Text(
+                                'No Recents',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black26),
+                                textAlign: TextAlign.center,
+                              ),
+                              loadingIndicator: const SizedBox.shrink(),
+                              tabIndicatorAnimDuration: kTabScrollDuration,
+                              categoryIcons: const CategoryIcons(),
+                              buttonMode: ButtonMode.MATERIAL,
+                              checkPlatformCompatibility: true,
+                            ),
+                          )),
+                    ),
+                  ],
+                ),
+            }));
+  }
+
+  //Getting the old messages from fireStore and placing the null item in the _messageList
   //Null item causes the loadingIndicator to appear in the list.
   //loading var is also added to if condition to stop the listener from calling firebase multiple times.
   void _scrollListener() async {
@@ -80,102 +234,45 @@ class _ChatScreenState extends State<ChatScreen> {
         !_dbService.loading) {
       final list = await _dbService.getOldMessageListSnapshot();
       if (list != null && list.isNotEmpty) {
-        if (messageList.contains(null)) messageList.remove(null);
-        // messageList.addAll(list);
-        int j = messageList.length - 1;
+        if (_messageList.contains(null)) _messageList.remove(null);
+        // _messageList.addAll(list);
+        int j = _messageList.length - 1;
         for (int i = 0; i < list.length; i++) {
-          if (messageList[j]!.id != list[i].id) messageList.add(list[i]);
+          if (_messageList[j]!.id != list[i].id) _messageList.add(list[i]);
           j--;
         }
-        setState(() => messageList.add(null));
+        setState(() => _messageList.add(null));
       } else {
         setState(() {
-          if (messageList.contains(null)) messageList.remove(null);
+          if (_messageList.contains(null)) _messageList.remove(null);
         });
       }
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: GestureDetector(
-            child: Icon(Icons.account_circle_rounded),
-            onTap: () => Navigator.push(context,
-                MaterialPageRoute(builder: (context) => ProfileScreen())),
-          ),
-          leadingWidth: 100, // default is 56
-          title: const Text('Chat Room'),
-          actions: [
-            IconButton.filled(
-                onPressed: () {
-                  widget._auth.signOut();
-                },
-                icon: const Icon(Icons.logout)),
-          ],
-        ),
-        body: switch (_viewState) {
-          ViewState.loading => const Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SpinKitCubeGrid(
-                      color: Colors.red,
-                    ),
-                    Text(
-                      'Upload Photo. Please wait...',
-                      style: TextStyle(fontSize: 16, color: Colors.black),
-                    )
-                  ]),
-            ),
-          ViewState.viewVisible => Column(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.black12,
-                    ),
-                    child: ListView.builder(
-                        itemCount: messageList.length,
-                        controller: _scrollController,
-                        reverse: true,
-                        itemBuilder: (context, index) {
-                          return MessageWidget(msg: messageList[index]);
-                        }),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          controller: messageController,
-                          decoration: InputDecoration(
-                              labelText: StringConstants.typeMessage),
-                          textInputAction: TextInputAction.search,
-                          onSubmitted: (value) =>
-                              _sendMessage(_scrollController),
-                        ),
-                      ),
-                      GestureDetector(
-                        child: const Icon(Icons.attach_file),
-                        onTapDown: (TapDownDetails details) async {
-                          _showPopupMenu(details.globalPosition);
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.send),
-                        onPressed: () => _sendMessage(_scrollController),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-        });
+  _onBackspacePressed() {
+    messageController
+      ..text = messageController.text.characters.toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: messageController.text.length));
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      if (_focus.hasFocus) emojiShowing = false;
+    });
+  }
+
+  //Method called when the back button of the device is presed.
+  _onBackButtonPressed() async {
+    if (_keyboardVisible) {
+      _focus.unfocus();
+      return false;
+    } else if (emojiShowing) {
+      setState(() => emojiShowing = false);
+      return false;
+    }
+    return true;
   }
 
   void _sendMessage(ScrollController controller) {
